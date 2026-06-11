5749133be730bafcb72c61df75bc9582b90edcdf
Rahil Siddiqui <rahilsid62@gmail.com>
2026-06-11 13:52:56 +0530

    huggnng face deployment

--- app.py
+++ app.py
@@ -206,4 +206,5 @@ def uploaded(filename):
 if __name__ == '__main__':
     print('Hybrid (EfficientNetB0+ResNet50) app running at http://localhost:5000')
     print(f'Device: {DEVICE}')
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', '7860'))
+    app.run(host='0.0.0.0', port=port, debug=False)
